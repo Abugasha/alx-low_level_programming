85f3a96de29c1528eaea4259913a564ae0beb273
Abugasha <shihabunimap@gmail.com>
2022-10-13 17:45:56 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
+
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *  *
- *
- *  * Return: always 0
- *
- *  */
-
+ * Return: always 0 (succcess)
+ */
 int main(void)
-
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-	        return (0);
-
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
