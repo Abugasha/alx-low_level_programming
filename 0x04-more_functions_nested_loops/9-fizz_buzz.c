be4200f10d9fd783175aaccc6903c444ce2e0f8e
Abugasha <shihabunimap@gmail.com>
2022-10-20 18:53:28 +0000

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,34 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_square - prints square stuff
+ * main - main function
  *
- * @size: integer to set square size
+ *
+ * Return: a number
  */
-void print_square(int size)
+int main(void)
 {
 	int i;
-	int j;
 
-	if (size <= 0)
+	for (i = 1; i <= 100; i++)
 	{
-		_putchar('\n');
-	} else
-	{
-		for (i = 0; i < size; i++)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			for (j = 0; j < size; j++)
+			if (i % 3 == 0)
 			{
-				_putchar('#');
+				printf("Fizz");
 			}
-			_putchar('\n');
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
+		{
+			printf("%d", i);
 		}
+		if (i != 100)
+			printf(" ");
 	}
+	printf("\n");
+	return (0);
 }
