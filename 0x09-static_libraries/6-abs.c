9342ed325ab7e1f608b1bd083af41d32c5a0ccb5
Abugasha <shihabunimap@gmail.com>
2022-11-10 01:16:03 +0000

    abs

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - checks if a number is positive, negative or zero
+ * @n: number to evalue
+ * Return: 1 absolute number value.
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	return (n);
+}
