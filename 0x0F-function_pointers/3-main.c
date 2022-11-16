9086aca1de2029bb3647fe6094b394b077e9a388
Abugasha <shihabunimap@gmail.com>
2022-11-16 20:20:44 +0000

    last mandorty

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+/**
+ *main - console calculator using pointer to functions
+ *@argc: quantity of arguments
+ *@argv: data of arguments
+ *Return: Execution status
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*pointer_to_function)(int, int);
+
+	if (argc - 1 != 3  || argv[1] == NULL
+			|| argv[3] == NULL || argv[2] == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+			&&  *argv[2] != '/' &&  *argv[2] != '%') || strlen(argv[2]) != 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	pointer_to_function = get_op_func(argv[2]);
+	if (pointer_to_function == NULL)
+		return (0);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = (*pointer_to_function)(a, b);
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ *op_add - adds two values
+ *@a: first value
+ *@b: second value
+ *Return: sum result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - substract value a - b
+ *@a: first value
+ *@b: second value
+ *Return: substract result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiply two values
+ *@a: first value
+ *@b: second value
+ *Return: Mult result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - returns the division result
+ *if b == 0 returns null
+ *@a: numerator
+ *@b: denominator
+ *Return: division result
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ *op_mod - returns the mod between a and b
+ *checks if b == 0
+ *@a: denominator
+ *@b: dividen
+ *Return: reminder of a / b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+
+}
