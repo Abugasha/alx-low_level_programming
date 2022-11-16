c1426afe9625192c99eb658417f6decab3096211
Abugasha <shihabunimap@gmail.com>
2022-11-16 20:08:49 +0000

    print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ *print_name - prints the name of a person using a requested
+ *function
+ *@name: person name
+ *@f: pointer to function
+ *Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+		(*f)(name);
+}
