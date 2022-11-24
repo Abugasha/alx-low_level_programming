51e356bb8e9f70521a639328e1978fe5e2cabdb3
Abugasha <shihabunimap@gmail.com>
2022-11-24 23:58:25 +0000

    added all

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *list_len - gives the length of a list
+ *@h: pointer to the first node
+ *Return: lenght of the list
+ */
+size_t list_len(const list_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+ #include "lists.h"
+/**
+ *add_node - adds new node at end of list
+ *@head: pointer to the pointer of the list
+ *@str: str data to add
+ *Return: pointer to the new element of list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int len_str = 0;
+	list_t *new_node;
+	char *final = NULL;
+
+	if (head == NULL)
+		return (NULL);
+	if (str != NULL)
+	{
+		final = strdup(str);
+		if (final == NULL)
+			return (NULL);
+		while (final[len_str])
+			len_str++;
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = final;
+	new_node->len = len_str;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
